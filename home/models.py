e2150f445b9fc621eb2e20eeb518708e7d241902
habib_sj <asbchobi@gmail.com>
2023-05-07 19:32:52 +0430

    create new app home and etc

---
+++ home/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=1000)
+    content = models.TextField()
+    slug = models.SlugField(max_length=1000)
+    image = models.ImageField(upload_to='blog')
+    created_at = models.DateTimeField(auto_now_add=True)
+    upload_to = models.DateTimeField(auto_now=True)
+
+
---
+++ home/urls.py
@@ -0,0 +1,6 @@
+from froala_editor import views
+
+
+urlpatterns = [
+    path('froala_editor/', include('froala_editor.urls'))
+]
